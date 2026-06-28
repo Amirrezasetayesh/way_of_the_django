a0ab5dbb3e6d3144dd7b3211352ff060c8f58268
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-06-28 15:07:32 +0330

    i write some introductions about my codes and just it

--- part_one/views.py
+++ part_one/views.py
@@ -7,11 +7,12 @@ from django.shortcuts import render,HttpResponse
 def first_per(rq):
     return HttpResponse("<h1>the first person is amir</h1>")
 
-
+# HttpResponse just return some html code to persons like below code
 def home_page(rq):
     return render(rq,"for_home/first_index.html")
 
-
+# render is for your models and it can show some html pages to persons
+# render receives one rq (the rq of your def) and one location for you html file from the templates
 
 def second_per(rq):
     return HttpResponse("<h1> the second person is reza</h1>")
