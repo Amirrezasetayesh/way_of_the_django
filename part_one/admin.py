0275e000850b6c0b341f9e31eebb10103aa98e51
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-06-29 15:58:31 +0330

    i regist some model in admin file and i wrote some duc here

--- part_one/admin.py
+++ part_one/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Person,Personal_data
 
 # Register your models here.
+
+admin.site.register(Person)
+
+admin.site.register(Personal_data)
+
+
+#  if you want save and working you model in you site so you should write below code:
+# from .models import model_name
+# admin.site.register(model_name)
