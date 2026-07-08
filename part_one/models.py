238cb094c7396a706437e4998963a7c33167b0d6
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-07-08 11:08:59 +0330

    i change model name to Character and change some fields

--- part_one/models.py
+++ part_one/models.py
@@ -6,13 +6,14 @@ from django.db import models
 
 
 
-class Person(models.Model):
+class Character(models.Model):
 
     First_name=models.CharField(max_length=20)
 
     Last_name=models.CharField(max_length=20)
 
-    Gender=models.CharField(max_length=5)
+    gender_list=[("m","male"),("f","female")]
+    Gender=models.CharField(choices=gender_list)
 
     Height=models.CharField()
 
@@ -21,16 +22,18 @@ class Person(models.Model):
     Age=models.IntegerField(blank=True,null=True)
     # about blank if blank is true you can write nothing in you field
     # about null if null is true so you can write none in filed
-    Phone_number=models.CharField(blank=True,null=True)
+    Head_gift=models.CharField()
+    f_or_uf=[("f","friendly"),("unf","unfriendly")]
+    role_of_the_character=models.CharField(choices=f_or_uf)
 
-    Email=models.EmailField(max_length=80,blank=True,null=True)
+    f_level_list=[("y","YOnko"),("SH","ShiJeBoKay"),("n_p","normal_pirate"),("A","AdMiArl"),("s_a","second_AdMiRal"),("n_m","normal_marian")]
+    character_level=models.CharField(choices=f_level_list,default=f_level_list[-1])
 
-    User_Name=models.CharField(max_length=20,blank=True,null=True)
 
 
 
     def __str__(self):
-        return f"{self.First_name} {self.Last_name}"
+        return f"{self.First_name}"
 # if you write the def str in you models so you can see your method you chose in return (self.any_method) in the admin table
 
 class Personal_data(models.Model):
@@ -38,23 +41,17 @@ class Personal_data(models.Model):
     Home_address=models.CharField(blank=True,null=True)
 
     Office_Address=models.CharField(blank=True,null=True)
-
-    House_Phone=models.CharField(blank=True,null=True)
-
-    Emai_Password=models.CharField(blank=True,null=True)
-
-    Phone_password=models.CharField(blank=True,null=True)
 #     for now
-    photo_of_person=models.ImageField(upload_to="upload/photos",blank=True,null=True)
+    photo_of_the_character=models.ImageField(upload_to="upload/photos",blank=True,null=True)
     #     this method is for save and upload the photos
 
-    person=models.ForeignKey(Person,on_delete=models.CASCADE,default=1)
+    character=models.ForeignKey(Character,on_delete=models.CASCADE,default=1)
 #     when you make the person then you can use all of the object of the person class like first_name or last_name
 #     if on_delete is models.cascade so when you del the father model , after that child model del
 #     for use the object of person for personal_data models you should write somthing like below code:
 #     name of your model (the model make with personal_data).person.name of your some object from person like First_name
     def __str__(self):
-        return f"personal data of {self.person.First_name}"
+        return f"personal data of {self.character.First_name}"
 
 
 
