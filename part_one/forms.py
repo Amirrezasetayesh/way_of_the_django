ddfa82b96c9c29458dc210cf4c07174f5b92d885
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-07-18 11:52:46 +0330

    i write some help below and up site all of codes and just it

--- part_one/forms.py
+++ part_one/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class SignupForm(UserCreationForm):
     # UserCreationForm is import to views.py
+    #label == write somthing upside of your parameter
+    #widget give some attrs like(class , name , place holder ) and name is the name of your parameter and placeholder some word in yor textbox
     email = forms.EmailField(
         label="your email:",
         widget=forms.EmailInput(attrs={
@@ -31,6 +33,7 @@ class SignupForm(UserCreationForm):
 
         })
     )
+
     password2 = forms.CharField(
         label="you password:",
         widget=forms.PasswordInput(attrs={'class':'form-control',
@@ -42,7 +45,9 @@ class SignupForm(UserCreationForm):
     )
 
 
-
+    # if you want your class (from class) do somthing , you should make one Meta class
     class Meta:
+        # at the first you need one user model to use you form
         model = User
+        # you should set all  parameters of you form to some field and the order of this fields is order of how person can see your parameters
         fields = ( 'email', 'username', 'password1', 'password2')
--- part_one/views.py
+++ part_one/views.py
@@ -35,20 +35,28 @@ def gran_line(rq):
 
 def login_user(request):
     if request.method=="POST":
+        # we give the information from POST method pay attention we set the POST method in our html file
         username_user=request.POST['username']
+        # we give the username and password form the POST method datas
         password_user=request.POST['password']
 
 
         user=authenticate(request,username=username_user,password=password_user)
+        # authenticate method is for check the username and password of person for login
         if user is not None:
+            # if user does exist you should login the user in our site
             login(request,user)
+            # login method is done the
             messages.success(request,"login was successful")
+            # if the return of code was true you tell me the message
             return redirect("main_page")
+        # and now take me to main page please with redirect
         else:
             messages.success(request,"login was not successful")
+            # else tell me this messages
             return redirect("login_user_site")
     else:
-
+        # if you did not give any POST method from front end you should render and check the login template
         return render(request,'login.html')
 
 
@@ -63,15 +71,20 @@ def logout_user(request):
 
 def signup_user(request):
     form=SignupForm()
-
+    # at the first you should make you own form in this view, pay attention you before you call this form you should already make this form in forms.py
+    # you should make the forms.py in you app
     if request.method=="POST":
+        # again you should give the introductions from POST method and check it
         form=SignupForm(request.POST)
+        # you should pour the data from POST method to your own form
         if form.is_valid:
+            # if all the parameters is true and the person add the information to form , you should chek the form values=== is_valid
             form.save()
+            # and now you should save information for one user in site
             username=form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(request, username=username, password=password)
-
+            # this Process is like login or equal login Process
             login(request,user)
             messages.success(request,"signup was successful")
             return redirect("main_page")
@@ -79,4 +92,5 @@ def signup_user(request):
             messages.success(request,"login was not successful")
             return redirect("signup_user_site")
     else:
+        # if you do not give any data from POST method you can check again signup_user_site template
         return render(request,'signup.html',{'form':form})
