2862299b47dcd46dcec0830ed544c36fcf23d52e
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-07-30 18:11:32 +0330

    nothing happend

--- part_tow/views.py
+++ part_tow/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth import logout,login,authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django import forms
 
 
 
 
 def check_choices(request):
-    pass
+    return render(request,"choices_box.html",{})
 
 
 
 
 def select(request):
-    pass
+    return render(request,"choices_box.html",{})
 
 
 
 
 
 def not_select(request):
-    pass
+    return render(request,"choices_box.html",{})
 
 
 
 
 def select_again(request):
-    pass
+    return render(request,"choices_box.html",{})
