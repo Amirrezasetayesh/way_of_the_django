14f07fda2c179cef9c9ed382482b7dce7e779c05
reza_st <reza.2.0.0.1.yt@gmail.com>
2026-08-02 12:38:15 +0330

    this file is the body of box and we make one class here

--- part_tow/box_choices.py
+++ part_tow/box_choices.py
@@ -1,9 +1,13 @@
-class Cart:
+class Box:
     def __init__(self,request):
+        # request is the request of the user or customer of site
         self.session= request.session
-        cart=self.session.get('session_key')
+        # now jst we should set one  session attribute for box with up code
+        box=self.session.get('session_key')
+        # with up code we check the session part of the box for know about the product or targets on box user
 
         if 'session_key' not in request.session:
-            cart=self.session['session_key'] = {}
-        self.cart=cart
+            box=self.session['session_key'] = {}
+        self.box=box
 
+# the role of this file is our main box for choice some character in our site
